e790eef08b28cbe0767c757a2538af1b6948272f
jackwade <6219292+jackwade@users.noreply.github.com>
2019-05-04 11:55:01 -0700

    reviews route untested

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  if (!token) return res.status(401).json('Access denied. Try logging in first.');
 
   try {
     const decoded = jwt.verify(token, process.env.jwtPrivateKey);
@@ -10,6 +10,6 @@ module.exports = function (req, res, next) {
     next();
   }
   catch (ex) {
-    res.status(400).send('Invalid token.');
+    res.status(400).json('Invalid token.');
   }
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,48 +3,102 @@ const { User, Review } = require("../models");
 const express = require('express');
 const router = express.Router();
 
-router.get('/', auth, async (req, res) => {
-  // GET ONLY PRODUCT NAME?
-  const reviews = await Review.findAll({
-    where: {
-      userId: req.user.id,
-    },
+router.get('/:page', (req, res) => {
+  let page = parseInt(req.params.page) || 1;
+  const product = req.query.product;
+  const user = req.query.user;
+
+  const options = {
     include: [
       {
         model: User,
         through: {
           attributes: ['username', 'id'],
-        }
+        },
       },
     ],
-  });
- 
-  if (!reviews || !reviews[0]) return res.status(404).json('No reviews found.');
-  
-  res.status(200).json(reviews);
-  console.log('review list sent')
+  };
+
+  if (product) options.where.product = product;
+  if (user) options.where.userId = user;
+
+  sendReviews(options, page);  
 });
 
-router.get('/:id', auth, async (req, res) => {
-  console.log('reviews get by id')
+
+router.get('/me/:page', auth, (req, res) => {
+  let page = req.params.page;
+  const product = req.query.product;
+
+  const options = {
+    where: {
+      userId: req.user.id,
+    },
+  };
+
+  if (product) options.where.product = product;
+
+  sendReviews(options, page);
 });
+
+
+router.get('/:id', async (req, res) => {
+  const review = await Review.findOne({
+    where: {
+      id: req.params.id
+    },
+  });
+  
+  if (!review || !review[0]) return res.status(404).json(`Review not found.`);
+
+  res.status(200).json(review);  
+});
+
 
 router.post('/', auth, async (req, res) => {
+  // const { error } = validate(req.body);
+  // if (error) return res.status(400).json(error.details[0].message);
 
-  // user ID comes off the token
-  const userId = req.user.id;
-  const review = await Review.create(req.body.review);
+  const review = req.body.review;
+  review.userId = req.user.id;
+
+  const review = await Review.create(review);
 
   res.status(200).json(review);
-  console.log('review created');
 });
 
-router.put('/:id', auth, async (req, res) => {
-  console.log('reviews put')
-});
+// router.put('/:id', auth, async (req, res) => {
+//   // const { error } = validate(req.body); 
+//   // if (error) return res.status(400).json(error.details[0].message);
 
-router.delete('/:id', auth, async (req, res) => {
-  console.log('reviews delete')
-});
+//   console.log('reviews put')
+// });
+
+// router.delete('/:id', auth, async (req, res) => {
+//   console.log('reviews delete')
+// });
+
+sendReviews(moreOptions, page) {
+  const limit = 50;
+  let offset = page * limit;
+
+  let options = {
+    attributes: ['id', 'product', 'rating', 'imgUrl'],
+    limit,
+    offset,
+    $sort: { id: 1 },
+  };
+
+  if (moreOptions) options = Object.assign(options, moreOptions);
+
+  const result = await Review.findAndCountAll(options);
+
+  if (!result || !result.reviews || !result.reviews[0]) return res.status(404).json('No reviews found.');
+
+  const pages = Math.ceil(reviews.count / limit);
+  // offset = limit * (page - 1);
+  const reviews = result.rows;
+  res.status(200).json({ reviews, 'count': result.count, pages });
+}
 
 module.exports = router;
